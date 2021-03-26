3ccd1432a839cabe75de235b0ef2db84974bd6b6
SyrusVir <jude.alnas@gmail.com>
2021-03-25 23:33:43 -0500

    TDC test code (tdc.c) complete

---
+++ 8bitTo32bit_test.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+uint32_t convertSubsetToLong(uint8_t* start, int len, bool big_endian)
+{
+    /**Parameters: uint8_t* start - pointer to first element in byte array subset
+     *             int len - number of elements, max 4, in the subset
+     *             bool big_endian - if true, the first element of start is considerd the MSB
+     * Returns: 
+     */
+
+    len = (len > 4 ? 4 : len); //if len > 4, assign 4. OTW assign user-provided length
+    uint32_t out = 0;
+    for (int i = 0; i < len; i++)
+    {
+        out |= (start[(big_endian ? len - 1 - i : i)] << 8*i);
+    }
+
+    return out;
+}
+
+int main ()
+{
+    uint8_t data[13] = {0x00, 0x04, 0x04, 0x30, 0x22, 0xFF,
+                        0x34, 0x34, 0x9f, 0xEE, 0xA2, 0x5C, 0x87};
+
+    printf("%X\n",convertSubsetToLong(data+1,3,1));
+    printf("\n");
+    printf("%X\n",convertSubsetToLong(data+1,3,0));
+
+    uint8_t test[4] = {0x2};
+
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%hu ", test[i]);
+    }
+
+} 
