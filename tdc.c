60d5db6984c9462d6c5f031d89da56f9d2db546e
SyrusVir <jude.alnas@gmail.com>
2021-03-29 15:40:24 -0500

    Non-working SPI in tdc.c

--- tdc.c
+++ tdc.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define AUTOINC_METHOD
+// #define AUTOINC_METHOD
 
 //TDC register addresses; easier to define using enum
 enum TDC_REG_ADDR {
@@ -34,12 +34,14 @@ enum TDC_REG_ADDR {
 #define TDC_CMD(auto_inc, write, tdc_addr) (auto_inc << 7) | (write << 6) | (tdc_addr)
 
 //TEST definitions
-#define TDC_ENABLE_PIN 0
-#define TDC_INT_PIN 0
-#define TDC_BAUD (uint32_t)20E6
-#define TDC_START_PIN 0         // physical pin #; provides TDC start signal for debugging 
-#define TDC_STOP_PIN 0         // physical pin #; provides TDC stop signal for debugging 
-#define TDC_TIMEOUT_USEC 500
+#define TDC_CLK_PIN 4   // physical pin 7; GPIOCLK0 for TDC reference
+#define TDC_ENABLE_PIN 27   // physical pin 13; TDC Enable
+#define TDC_INT_PIN 22
+#define TDC_BAUD (uint32_t)10E6
+#define TDC_START_PIN 23         // physical pin 16; provides TDC start signal for debugging 
+#define TDC_STOP_PIN 18         // physical pin 12; provides TDC stop signal for debugging 
+#define TDC_TIMEOUT_USEC (uint32_t)5E6
+#define TDC_CLK_FREQ (uint32_t)19.2E6/2
 
 typedef struct TDC {
     uint8_t clk_pin;    // Proivdes TDC reference clock
@@ -49,6 +51,14 @@ typedef struct TDC {
     int spi_handle;
 } tdc_t;
 
+void printArray(char* arr, int arr_size)
+{
+    for (int i = 0; i < arr_size; i++)
+    {
+        printf("%X ", arr[i]);
+    }
+    printf("\n");
+}
 //convert a subset of a byte array into a 32-bit number
 uint32_t convertSubsetToLong(char* start, int len, bool big_endian)
 {
@@ -72,17 +82,21 @@ int main ()
 {
     tdc_t tdc = {
         .enable_pin = TDC_ENABLE_PIN,
-        .int_pin = TDC_INT_PIN
+        .int_pin = TDC_INT_PIN,
+        .clk_pin = TDC_CLK_PIN,
+        .clk_freq = TDC_CLK_FREQ
     };
     
+    gpioInitialise();
+
     gpioSetMode(tdc.enable_pin, PI_OUTPUT);     // active LOW
     gpioSetMode(tdc.int_pin, PI_INPUT);         // active LOW        
     gpioSetPullUpDown(tdc.int_pin, PI_PUD_UP);  // INT pin is open drain, active LOW
     gpioSetMode(TDC_START_PIN, PI_OUTPUT);     // active LOW
     gpioSetMode(TDC_STOP_PIN, PI_OUTPUT);     // active LOW
     
-    tdc.spi_handle = spiOpen(0, TDC_BAUD,
-            0b00 |          // Positive (MSb 0) clock edge centered (LSb 0) on data bit
+    tdc.spi_handle = spiOpen(0, TDC_BAUD, 0
+            /* 0b00 |          // Positive (MSb 0) clock edge centered (LSb 0) on data bit
             (0b000 << 2) |  // all 3 CE pins are active low
             (0b000 << 5) |  // all 3 CE pins reserved for SPI
             (0 << 8) |      // 0 = Main SPI; 1 = Aux SPI 
@@ -90,12 +104,14 @@ int main ()
             ((0 & 0xF) << 10) | // bytes to write before switching to read (N/A if not in 3-wire mode)
             (0 << 14) |         // If 1, tx LSb first (Aux SPI only)
             (0 << 15) |         // If 1, recv LSb first (Aux SPI only)
-            ((8 & 0x3F) << 16)  // bits per word; default 8
+            ((8 & 0x3F) << 16)  // bits per word; default 8 */
 
     );
 
+    printf("tdc.spi_handle=%d\n",tdc.spi_handle);
     gpioHardwareClock(tdc.clk_pin,tdc.clk_freq);
-    
+    gpioWrite(tdc.enable_pin,1);
+    gpioDelay(50);
     gpioWrite(tdc.enable_pin, 0);
 
     //Non-incrementing write to CONFIG2 reg (address 0x01)
@@ -103,7 +119,8 @@ int main ()
     // no averaging, and single stop signal operation
     uint8_t cal_periods = 2;
     char tx_buff[] = {0x41, 0x00};
-    spiWrite(tdc.spi_handle, tx_buff, sizeof(tx_buff));
+    printArray(tx_buff, sizeof(tx_buff));
+    printf("spiWrite=%d\n",spiWrite(tdc.spi_handle, tx_buff, sizeof(tx_buff)));
     /****************************************/
     
     static char meas_commands[2] = {
@@ -116,22 +133,22 @@ int main ()
         char c;
         printf("Enter any character to begin a TDC measurement.\n");
         printf("Enter 'q' or 'Q' to quit.\n");
-        scanf(" %c ", &c);
+        scanf(" %c", &c);
         if (c == 'q' || c == 'Q') break;
 
         //start new measurement on TDC
         spiWrite(tdc.spi_handle, meas_commands, sizeof(meas_commands));
         gpioDelay(10); // small delay to allow TDC to process data
-        gpioTrigger(TDC_START_PIN, 1, 1);
+        gpioTrigger(TDC_START_PIN, 5, 1);
 
         //DEBUGGING: wait a know period of time and send a stop pulse
         gpioDelay(500);
-        gpioTrigger(TDC_STOP_PIN, 1, 1);
+        gpioTrigger(TDC_STOP_PIN, 5, 1);
 
         //Poll TDC INT pin to signal available data
         uint32_t curr_tick = gpioTick();
         uint32_t stop_tick = curr_tick + TDC_TIMEOUT_USEC;
-        while (gpioRead(tdc.int_pin) && curr_tick < stop_tick)
+        while (!gpioRead(tdc.int_pin) && curr_tick < stop_tick)
             curr_tick = gpioTick();
 
         if (curr_tick < stop_tick) //if TDC returned in time
@@ -181,11 +198,13 @@ int main ()
             uint32_t tdc_data[sizeof(tof_commands)]; // array to hold TIME1, CLOCK_COUNT1, TIME2, CLOCK_COUNT2, CALIBRATION1, CALIBRATION2 in that order
             for (int i = 0; i < sizeof(tof_commands); i++)
             {
-                uint8_t tx_buff1[4] = {tof_commands[i]} uint8_t rx_buff1[sizeof(tx_buff1)];
+                char tx_buff1[4] = {tof_commands[i]};
+                char rx_buff1[sizeof(tx_buff1)];
                 int bytes_xfer = spiXfer(tdc.spi_handle, tx_buff1, rx_buff1, sizeof(tx_buff1));
-                print("rx_buff1 = %s\n", rx_buff1);
-                tdc_data[i] = convertSubsetToLong(rx_buff1)
-                    print("rx_buff1 converted= %u\n", tdc_data[i]);
+                printf("bytes_xfer=%d\n",bytes_xfer);
+                printf("rx_buff1 = %s\n", rx_buff1);
+                tdc_data[i] = convertSubsetToLong(rx_buff1, sizeof(rx_buff1),true);
+                printf("rx_buff1 converted= %u\n", tdc_data[i]);
             }
 
             time1 = tdc_data[0];
@@ -216,4 +235,6 @@ int main ()
             printf("TDC timeout occured\n");
         }
     } // end while(1)
+
+    spiClose(tdc.spi_handle);
 } // end main()
